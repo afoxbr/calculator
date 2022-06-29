003e2167edcaf4f2e75cdf5494a5333fc962ba58
AntonyFB <antonyrbramwell@gmail.com>
2022-06-29 20:51:58 +0100

    Creat basic UI, work on JS

--- scripts.js
+++ scripts.js
@@ -1,3 +1,6 @@
+const display = document.querySelector('.display');
+  display.textContent = "0";
+
 function add(a, b) {
     return (a + b);
 };
@@ -13,3 +16,48 @@ function multiply(a, b) {
 function divide(a, b) {
     return (a / b);
 ;}
+
+function operate(a, operator, b) {
+    display.textContent = (a + " " + operator + " " + b + " = " (operator(a, b)));
+}
+
+const butt1 = document.querySelector('.b1');
+  butt1.addEventListener("click", () => {
+    if (operate(a) = null) {
+        operate(a) = 1;
+    } else {
+        operate(b) = 1;
+    }
+  });
+
+const butt2 = document.querySelector('.b2');
+
+const butt3 = document.querySelector('.b3');
+
+const butt4 = document.querySelector('.b4');
+
+const butt5 = document.querySelector('.b5');
+
+const butt6 = document.querySelector('.b6');
+
+const butt7 = document.querySelector('.b7');
+
+const butt8 = document.querySelector('.b8');
+
+const butt9 = document.querySelector('.b9');
+
+const butt0 = document.querySelector('.b0');
+
+const buttClear = document.querySelector('.clear');
+
+const buttDec = document.querySelector('.decimal');
+
+const buttAdd = document.querySelector('.add');
+
+const buttSub = document.querySelector('.subtract');
+
+const buttMul = document.querySelector('.multiply');
+
+const buttDiv = document.querySelector('.divide');
+
+const buttOp = document.querySelector('.operate');
