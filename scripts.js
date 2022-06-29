674d9802dbcb9f9df365a1a649b14bb1063ae981
AntonyFB <antonyrbramwell@gmail.com>
2022-06-29 21:00:41 +0100

    Event listeners; undefined ATM

--- scripts.js
+++ scripts.js
@@ -22,42 +22,52 @@ function operate(a, operator, b) {
 }
 
 const butt1 = document.querySelector('.b1');
-  butt1.addEventListener("click", () => {
-    if (operate(a) = null) {
-        operate(a) = 1;
-    } else {
-        operate(b) = 1;
-    }
-  });
+  butt1.addEventListener("click", );
 
 const butt2 = document.querySelector('.b2');
+  butt2.addEventListener("click", );
 
 const butt3 = document.querySelector('.b3');
+  butt3.addEventListener("click", );
 
 const butt4 = document.querySelector('.b4');
+  butt4.addEventListener("click", );
 
 const butt5 = document.querySelector('.b5');
+  butt5.addEventListener("click", );
 
 const butt6 = document.querySelector('.b6');
+  butt6.addEventListener("click", );
 
 const butt7 = document.querySelector('.b7');
+  butt7.addEventListener("click", );
 
 const butt8 = document.querySelector('.b8');
+  butt8.addEventListener("click", );
 
 const butt9 = document.querySelector('.b9');
+  butt9.addEventListener("click", );
 
 const butt0 = document.querySelector('.b0');
+  butt0.addEventListener("click", );
 
 const buttClear = document.querySelector('.clear');
+  buttClear.addEventListener("click", );
 
 const buttDec = document.querySelector('.decimal');
+  buttDec.addEventListener("click", );
 
 const buttAdd = document.querySelector('.add');
+  buttAdd.addEventListener("click", );
 
 const buttSub = document.querySelector('.subtract');
+  buttSub.addEventListener("click", );
 
 const buttMul = document.querySelector('.multiply');
+  buttMul.addEventListener("click", );
 
 const buttDiv = document.querySelector('.divide');
+  buttDiv.addEventListener("click", );
 
 const buttOp = document.querySelector('.operate');
+  buttOp.addEventListener("click", );
